370a6c49937cf5b6d162185bfbeec791cd42a444
surendar1725 <42261186+surendar1725@users.noreply.github.com>
2019-08-25 17:00:08 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -28,7 +28,7 @@ def main():
         return                                                          # and exit program
     # end if
 
-    imgOriginalScene  = cv2.imread("3.jpeg")               # open image
+    imgOriginalScene  = cv2.imread("2.jpeg")               # open image
 
     if imgOriginalScene is None:                            # if image was not read successfully
         print("\nerror: image not read from file \n\n")  # print error message to std out
